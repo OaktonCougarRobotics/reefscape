8734edef60070a70b4a8538b58e03cc437e29239
Lamborghini Bugatti <yanismehenni2009@gmail.com>
2025-01-31 19:36:02 -0500

    pathplanner added

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -14,10 +14,12 @@ import java.util.function.DoubleSupplier;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.units.measure.MutDistance;
 import edu.wpi.first.units.measure.MutLinearVelocity;
 import edu.wpi.first.units.measure.MutVoltage;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -25,6 +27,18 @@ import frc.robot.Constants.OperatorConstants;
 import swervelib.SwerveDrive;
 import swervelib.math.SwerveMath;
 import swervelib.parser.SwerveParser;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+import com.pathplanner.lib.commands.PathfindingCommand;
+import com.pathplanner.lib.config.PIDConstants;
+import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+import com.pathplanner.lib.path.PathConstraints;
+import com.pathplanner.lib.path.PathPlannerPath;
+import com.pathplanner.lib.util.DriveFeedforwards;
+import com.pathplanner.lib.util.swerve.SwerveSetpoint;
+import com.pathplanner.lib.util.swerve.SwerveSetpointGenerator;
 
 public class Drivetrain extends SubsystemBase {
   /**
@@ -116,11 +130,81 @@ public class Drivetrain extends SubsystemBase {
           false);
     });
   }
-
+  public Pose2d getPose(){
+    return swerveDrive.getPose();
+  }
+  public void resetOdometry(Pose2d pose){
+    swerveDrive.resetOdometry(pose);
+  }
   public void zeroGyro() {
     swerveDrive.zeroGyro();
   }
+  public ChassisSpeeds getRobotVelocity(){
+    return swerveDrive.getFieldVelocity();
+  }
+  public void setupPathPlanner() {
+      // Load the RobotConfig from the GUI settings. You should probably
+      // store this in your Constants file
+      RobotConfig config;
+      try {
+        config = RobotConfig.fromGUISettings();
 
+        final boolean enableFeedforward = true;
+        // Configure AutoBuilder last
+        AutoBuilder.configure(
+            this::getPose,
+            // Robot pose supplier
+            this::resetOdometry,
+            // Method to reset odometry (will be called if your auto has a starting pose)
+            this::getRobotVelocity,
+            // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+            (speedsRobotRelative, moduleFeedForwards) -> {
+              if (enableFeedforward) {
+                swerveDrive.drive(
+                    speedsRobotRelative,
+                    swerveDrive.kinematics.toSwerveModuleStates(speedsRobotRelative),
+                    moduleFeedForwards.linearForces());
+              } else {
+                swerveDrive.setChassisSpeeds(speedsRobotRelative);
+              }
+            },
+            // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds. Also
+            // optionally outputs individual module feedforwards
+            new PPHolonomicDriveController(
+                // PPHolonomicController is the built in path following controller for holonomic
+                // drive trains
+                new PIDConstants(5.0, 0.0, 0.0),
+                // Translation PID constants
+                new PIDConstants(5.0, 0.0, 0.0)
+            // Rotation PID constants
+            ),
+            config,
+            // The robot configuration
+            () -> {
+              // Boolean supplier that controls when the path will be mirrored for the red
+              // alliance
+              // This will flip the path being followed to the red side of the field.
+              // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+
+              var alliance = DriverStation.getAlliance();
+              if (alliance.isPresent()) {
+                return alliance.get() == DriverStation.Alliance.Red;
+              }
+              return false;
+            },
+            this
+        // Reference to this subsystem to set requirements
+        );
+
+      } catch (Exception e) {
+        // Handle exception as needed
+        e.printStackTrace();
+      }
+
+      // Preload PathPlanner Path finding
+      // IF USING CUSTOM PATHFINDER ADD BEFORE THIS LINE
+      PathfindingCommand.warmupCommand().schedule();
+    }
   public static double deadzone(double num, double deadband) {
     if (Math.abs(num) < deadband)
       return 0.0;

69e4eb73a6231d1d758433a7c1182495c1c4b746
SeungwooDY <sdyoon0701@gmail.com>
2025-02-14 19:55:19 -0500

    deleted Vision.java

--- src/main/java/frc/robot/Vision.java
+++
@@ -1,8 +0,0 @@
-package frc.robot;
-
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-
-public class Vision {
-    Pose2d pose1 = new Pose2d(0.0, 0.0, new Rotation2d(0.0)); // we can define setpoints (pose2ds) here
-}
